5604e88fcf2f99cf7b4b80bb288b8f34a44bd627
Nolva <1090034020@qq.com>
2020-08-10 23:52:36 +0800

    20% of mall project

---
+++ mall-gateway/src/main/java/com/nolva/mallgateway/MallGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.nolva.mallgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * 开启服务注册/发现(配置nacos的注册中心地址)
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallGatewayApplication.class, args);
+    }
+
+}
